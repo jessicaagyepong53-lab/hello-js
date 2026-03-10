1439318da5fb61a6755415425fdcf8e48052fbc5
Agyepong Jessica <jessicaagyepong53@gmail.com>
2026-03-10 08:57:43 +0000

    Beginner class backend

---
+++ intro.js
@@ -0,0 +1,8 @@
+// intro.js
+
+const name = "Agyepong Jessica";
+const age = 26;
+const favoriteLanguage = "JavaScript";
+console. log ("Agyepong Jessica:" name);
+console.log("26:", age)
+console.log("JavaScript", favoriteLanguage);
